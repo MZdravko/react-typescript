51a198deb6fe36e79c83f158c0a45c12413c230a
MZdravko <milan.zdravko019@gmail.com>
2021-11-08 00:30:25 +0100

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "./components/Button";
+import { Container } from "./components/Container";
+import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Input } from "./components/Input";
+import { Oscar } from "./components/Oscar";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
+import { Counter } from "./components/state/Counter";
+import { Loggedin } from "./components/state/Loggedin";
+//import { User } from "./components/state/User";
+import { Status } from "./components/Status";
+import { ThemeContextProvider } from './components/context/ThemeContext'
+import { Box } from './components/context/Box' 
+import { UserContextProvider } from "./components/context/UserContext";
+import { User } from "./components/context/User";
+import { MutableRef } from "./components/ref/MutableRef";
 
 function App() {
+
+  const personName = {
+    first: "Petar",
+    last: "Petrovic",
+  };
+
+  const personNamesList = [
+    {
+      first: "Mika",
+      last: "Mikic",
+    },
+    {
+      first: "Zika",
+      last: "Zikic",
+    },
+    {
+      first: "Pera",
+      last: "Peric",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* 
+      <Greet name={"Milan"} messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={personNamesList} />
+      
+      <Status status={"success"} />
+      <Heading>Hello World</Heading>
+
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Milan</Heading>
+      </Oscar>
+
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked!", event, id);
+        }}
+      />
+
+      <Input handleChange={(event) => console.log(event.target.value)} />
+
+      <Container style={{ border: "1px solid black", padding: "1rem" }} />
+      
+      <Loggedin />
+      <User />
+      
+      <Counter />
+
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
+      
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
+
+      */}
+
+      <MutableRef />
+
     </div>
   );
 }
